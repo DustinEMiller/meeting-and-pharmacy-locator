bf799c13210f4f7ec452295fd82ed340e2f22f24
unknown <dustinmiller032@gmail.com>
2016-07-19 10:36:50 -0500

    adding functionalty to disallow/allow dates after 10/1

--- zip/api/Models/Meeting.php
+++ zip/api/Models/Meeting.php
@@ -64,8 +64,17 @@ class Meeting {
     private function seminar()
     {
         $inParams = implode(',', array_fill(0, count($this->zipCodes), '?'));
-        $qry = $this->_db->prepare('SELECT location, campaign_name, address, address2, city, zip, start_date, 
-            time, state, campaign_id FROM askshirley.seminars where zip IN ('.$inParams.') ORDER BY start_date, time ASC' );
+        $now = new DateTime('now');
+        $upperBound = new DateTime('Oct 1');
+        $lowerBound = new DateTime('Dec 31');
+
+        if($now >== $upperBound && $now <== $lowerBound) {
+            $qry = $this->_db->prepare('SELECT location, campaign_name, address, address2, city, zip, start_date, 
+                time, state, campaign_id FROM askshirley.seminars where zip IN ('.$inParams.') ORDER BY start_date, time ASC' );
+        } else {
+            $qry = $this->_db->prepare('SELECT location, campaign_name, address, address2, city, zip, start_date, 
+                time, state, campaign_id FROM askshirley.seminars where zip IN ('.$inParams.') AND month(start_date) < 10 ORDER BY start_date, time ASC' );    
+        }
 
         foreach ($this->zipCodes as $k => $zipCode) {
             $qry->bindValue(($k+1), $zipCode);
