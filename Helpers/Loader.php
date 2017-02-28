02549440894c6fb54d952e9fe7660c6eb2c4b51e
DustinEMiller <dustinmiller032@gmail.com>
2017-02-27 19:36:54 -0600

    prepare for dev testing

--- Helpers/Loader.php
+++ Helpers/Loader.php
@@ -32,7 +32,7 @@ class Loader {
         }*/
 
         try {
-            require __DIR__ . DIRECTORY_SEPARATOR .'../Controllers/' . $this->controllerName . '.php';
+            require __DIR__ . '/../controllers/' . $this->controllerName . '.php';
         } catch (Exception $e) {
             return $e;
         }
