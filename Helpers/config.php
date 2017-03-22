91d6e9faf010405c7cc8d3dd704f5ad3b95e57ef
DustinEMiller <dustinmiller032@gmail.com>
2017-03-22 14:14:55 -0500

    more config

--- Helpers/config.php
+++ Helpers/config.php
@@ -10,7 +10,7 @@
 		'sf.lead.url' => get_cfg_var('salesforce.lead.url'),
 		'sf.record.type' => get_cfg_var('salesforce.record.type'),
 		'sf.seminar.report' => get_cfg_var('salesforce.seminar.report'),
-		'sf.password.notification' => get_cfg_var('salesforce.password.notification')'/services/data/v37.0/analytics/reports/',
+		'sf.password.notification' => get_cfg_var('salesforce.password.notification'),
 		'log' => get_cfg_var('shirley.log'),
 		'ses.sender' => get_cfg_var('ses.sender'),
 		'ses.user' => get_cfg_var('ses.user'),
