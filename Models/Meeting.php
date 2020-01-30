1927e0b0557bfed09d3d4c4533b5238f8af0247a
dustinemiller <dustinmiller032@gmail.com>
2020-01-30 10:14:18 -0600

    campaign id

--- Models/Meeting.php
+++ Models/Meeting.php
@@ -22,7 +22,7 @@ class Meeting
     private $campaignId;
     private $debug;
 
-    public function __construct($pdo, $zipCodes, $brandArray, $campaignArray)
+    public function __construct($pdo, $zipCodes, $brandArray, $campaignId)
     {
         $this->_connection = $pdo;
         $this->_db = $this->_connection->getDb();
@@ -34,8 +34,8 @@ class Meeting
             $this->debug = "brand = " . $this->brand;
         }
 
-        if(!empty($campaignId)) {
-            $this->campaignId = urldecode($campaignArray[0]);
+        if($campaignId) {
+            $this->campaignId = urldecode($campaignId);
         }
 
         $this->debug .= $campaignId;
